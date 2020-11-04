9716eb508780ff68edc3e6e4b648584020fe542a
Roger <rbaroudy@hotmail.com>
2020-11-04 14:23:07 +0200

    Internal Clock Flag added & Clock functions merged

--- src/variants/R9MM/variant.cpp
+++ src/variants/R9MM/variant.cpp
@@ -119,45 +119,46 @@ extern "C" {
   * @retval None
   */
   
-//64MHZ Internal Clock  
-// WEAK void SystemClock_Config(void)
-// {
-  // RCC_OscInitTypeDef RCC_OscInitStruct;
-  // RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  // RCC_PeriphCLKInitTypeDef PeriphClkInit;
+#if USE_INTERNAL_CLOCK
+// HSI config, 64MHZ Internal Clock
+ void SystemClock_Config(void)
+ {
+   RCC_OscInitTypeDef RCC_OscInitStruct;
+   RCC_ClkInitTypeDef RCC_ClkInitStruct;
+   RCC_PeriphCLKInitTypeDef PeriphClkInit;
 
-  // /* Initializes the CPU, AHB and APB busses clocks */
-  // RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  // RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  // RCC_OscInitStruct.HSICalibrationValue = 16;
-  // RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  // RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
-  // RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
-  // if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
-    // Error_Handler();
-  // }
+   /* Initializes the CPU, AHB and APB busses clocks */
+   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+   RCC_OscInitStruct.HSICalibrationValue = 16;
+   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+     Error_Handler();
+   }
 
-  // /* Initializes the CPU, AHB and APB busses clocks */
-  // RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+   /* Initializes the CPU, AHB and APB busses clocks */
+   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
                                 // | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-  // RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  // RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  // RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  // RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  // if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
-    // Error_Handler();
-  // }
+   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+     Error_Handler();
+   }
 
-  // PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
-  // PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
-  // if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
-    // Error_Handler();
-  // }
-// }
+   PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC;
+   PeriphClkInit.AdcClockSelection = RCC_ADCPCLK2_DIV6;
+   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK) {
+     Error_Handler();
+   }
+ }
 
-
-//72 MHz with 24Mhz ext. clock input/// (R9MM RX configuration)
+#else
+//HSE config, 72 MHz with 24Mhz ext. clock input/// (R9MM RX configuration)
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
@@ -185,11 +186,16 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
+  SystemCoreClockUpdate();  //to make sure the clock variables are updated
+
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
 }
+#endif
+
+SystemCoreClockUpdate(); // to make sure the clock variables are updated
 
 #ifdef __cplusplus
 }
